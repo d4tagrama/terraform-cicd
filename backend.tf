d65675fa6a1ca2053ff9addc6aa94142c9dab2dd
Rodrigo Ramirez <roid85@gmail.com>
2025-04-04 18:44:08 -0600

    Update backend.tf (disabling S3 backend Terraform Cloud)

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-d4tagrama"
-    key    = "data/terraform.tfstate"
-    dynamodb_table = "terraform-lock-file"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-d4tagrama"
+#    key    = "data/terraform.tfstate"
+#    dynamodb_table = "terraform-lock-file"
+#    region = "us-east-1"
+#  }
+#}
